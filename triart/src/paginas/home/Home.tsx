6b9691cb38510de5094d61e8bf7d1bf864007ea4
Matues <mateus.q.d@gmail.com>
2022-10-03 15:38:22 -0300

    primeira versão home

--- triart/src/paginas/home/Home.tsx
+++ triart/src/paginas/home/Home.tsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import {Typography, Grid, Button} from '@material-ui/core';
+import { Box } from '@mui/material'
+
+import './Home.css';
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+               <Grid xs={12} className='center'> MENU INTERATIVO
+                </Grid>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>
+                            Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>
+                            Expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" className='botao'>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img className='imagem' src="https://i.imgur.com/JbgDXEk.png" alt="" />
+                </Grid>
+                {/* <Grid xs={12} className='postagens'>
+                </Grid> */}
+            </Grid>
+    </>
   )
 }
 
